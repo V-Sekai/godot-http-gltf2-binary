3a79d43cc5fe0cdea02d3a4293e92529e4187ae7
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2025-01-24 21:51:32 -0800

    Add GLTFDocumentExtension prototype.

---
+++ addons/http_glb_host/MSFT_texture_dds.gd
@@ -0,0 +1,20 @@
+extends GLTFDocumentExtension
+
+## This class implements the MSFT_texture_dds extension for GLTF documents.
+
+func _save_image_at_path(state: GLTFState, image: Image, file_path: String, image_format: String, _lossy_quality: float) -> Error:
+	if image_format == "image/vnd-ms.dds" and image and image.get_width() > 0 and image.get_height() > 0:
+		var gltf: Dictionary = state.json
+		if not gltf.has("extensionsUsed"):
+			gltf["extensionsUsed"] = []
+		if "MSFT_texture_dds" not in gltf["extensionsUsed"]:
+			gltf["extensionsUsed"].append("MSFT_texture_dds")
+		var file: FileAccess = FileAccess.open(file_path, FileAccess.WRITE)
+		if file:
+			var dds_data = load("image_to_dds.gd").new()._convert_image_to_dds(image, file_path)
+			file.store_buffer(dds_data)
+			file.close()
+			return OK
+		else:
+			return ERR_CANT_OPEN
+	return OK
