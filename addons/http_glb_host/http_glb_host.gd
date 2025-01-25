da80d951cf5c13532de588d614ec01a42dbe9727
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2025-01-24 22:49:58 -0800

    Refactor HTTP server implementation and improve type annotations for clarity

--- addons/http_glb_host/http_glb_host.gd
+++ addons/http_glb_host/http_glb_host.gd
@@ -6,14 +6,16 @@
 @tool
 extends EditorPlugin
 
+var glb_data: PackedByteArray = PackedByteArray()
+var http_server: TCPServer
+const PORT = 8081
+
 func _enter_tree():
-	var editor_settings = EditorInterface.get_editor_settings()
 	http_server = TCPServer.new()
-	var err_http = http_server.listen(PORT)
+	var err_http: Error = http_server.listen(PORT)
 	if err_http != OK:
 		push_error("HTTP Server start error: " + str(err_http))
 		return
-	is_running = true
 
 func _exit_tree():
 	if not http_server:
@@ -21,19 +23,13 @@ func _exit_tree():
 	http_server.stop()
 	http_server = null
 
-var glb_data: PackedByteArray = PackedByteArray()
-var http_server: TCPServer
-var is_running := false
-
-const PORT = 8081
-
 func _process(delta):
 	if not http_server or not http_server.is_connection_available():
 		return
 	var http_client: StreamPeerTCP = http_server.take_connection()
 	if not http_client:
 		return
-	var request
+	var request: String
 	if http_client.get_available_bytes() > 0:
 		request = http_client.get_utf8_string(http_client.get_available_bytes()).strip_edges()
 	else:
@@ -44,9 +40,9 @@ func _process(delta):
 		http_client.put_data(error_response.to_utf8_buffer())
 		http_client.disconnect_from_host()
 		return
-	var gltf_doc = GLTFDocument.new()
-	var state = GLTFState.new()
-	var error = gltf_doc.append_from_scene(get_editor_interface().get_edited_scene_root(), state)
+	var gltf_doc: GLTFDocument = GLTFDocument.new()
+	var state: GLTFState = GLTFState.new()
+	var error: Error = gltf_doc.append_from_scene(get_editor_interface().get_edited_scene_root(), state)
 	if error != OK:
 		glb_data = PackedByteArray()
 		push_error("GLTF export error: " + str(error))
@@ -58,7 +54,7 @@ func _process(delta):
 		http_client.put_data(response.to_utf8_buffer())
 		http_client.put_data(glb_data)
 	else:
-		var error_response = "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\nConnection: close\r\n\r\nGLB data not available."
+		var error_response: String = "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\nConnection: close\r\n\r\nGLB data not available."
 		http_client.put_data(error_response.to_utf8_buffer())
 	
 	http_client.disconnect_from_host()
