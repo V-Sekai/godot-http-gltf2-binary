99debd44ddaf45b1c436e5c35f1507e3b793aa11
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2025-01-25 16:23:00 -0800

    Refactor HTTP GLB host to improve request handling and add error response functions

--- addons/http_glb_host/http_glb_host.gd
+++ addons/http_glb_host/http_glb_host.gd
@@ -8,7 +8,7 @@ extends EditorPlugin
 
 var http_server: TCPServer
 const PORT = 8080
-var MSFT_texture_dds : GLTFDocumentExtension = preload("res://addons/http_glb_host/MSFT_texture_dds.gd").new()
+var MSFT_texture_dds: GLTFDocumentExtension = preload("res://addons/http_glb_host/MSFT_texture_dds.gd").new()
 var compatible: bool = false
 
 func _enter_tree():
@@ -28,61 +28,73 @@ func _exit_tree():
 	http_server = null
 
 func _process(delta):
-	if not http_server or not http_server.is_connection_available():
+	if http_server == null:
 		return
+	if not http_server.is_connection_available():
+		return
+	
 	var http_client: StreamPeerTCP = http_server.take_connection()
-	if not http_client:
+	if http_client == null:
 		return
-	var request: String
+	
+	var request: String = ""
 	if http_client.get_available_bytes() > 0:
 		request = http_client.get_utf8_string(http_client.get_available_bytes()).strip_edges()
 	else:
 		http_client.disconnect_from_host()
 		return
-
+	
 	if not request.begins_with("GET "):
-		var error_response = "HTTP/1.1 400 Bad Request\r\nContent-Type: text/plain\r\nConnection: close\r\n\r\nInvalid request."
-		http_client.put_data(error_response.to_utf8_buffer())
-		http_client.disconnect_from_host()
+		send_bad_request(http_client, "Invalid request.")
 		return
-
+	
 	var path_end = request.find(" HTTP/")
 	if path_end == -1:
 		path_end = request.length()
+	
 	var full_path = request.substr(4, path_end - 4).strip_edges()
 	var query_string = ""
 	var path = full_path
+	
 	if full_path.find("?") != -1:
 		var parts = full_path.split("?", false, 2)
 		path = parts[0]
 		query_string = parts[1]
+	
 	compatible = query_string.find("compatible") != -1
 	var glb_data: PackedByteArray = PackedByteArray()
+	
 	if path.is_empty() or path == "/":
 		var gltf_doc = GLTFDocument.new()
+		gltf_doc.image_format = "PNG"
 		if compatible:
-			gltf_doc.image_format = "DDS"
-		else:
-			gltf_doc.image_format = "PNG"
+			gltf_doc.image_format = "DDS" 
 		var state = GLTFState.new()
 		var flags = EditorSceneFormatImporter.IMPORT_USE_NAMED_SKIN_BINDS | EditorSceneFormatImporter.IMPORT_GENERATE_TANGENT_ARRAYS
 		var error = gltf_doc.append_from_scene(get_editor_interface().get_edited_scene_root(), state, flags)
-		if error == OK:
-			glb_data = gltf_doc.generate_buffer(state)
-		else:
+		if error != OK:
 			push_error("GLTF export error: " + str(error))
+			return
+		glb_data = gltf_doc.generate_buffer(state)
 	else:
-		var invalid_response = "HTTP/1.1 400 Bad Request\r\nContent-Type: text/plain\r\nConnection: close\r\n\r\nInvalid path."
-		http_client.put_data(invalid_response.to_utf8_buffer())
-		http_client.disconnect_from_host()
+		send_bad_request(http_client, "Invalid path.")
 		return
-
+	
 	if glb_data.size() > 0:
 		var response = "HTTP/1.1 200 OK\r\nContent-Type: model/gltf-binary\r\nContent-Disposition: attachment; filename=\"model.glb\"\r\nContent-Length: %d\r\nConnection: close\r\n\r\n" % glb_data.size()
 		http_client.put_data(response.to_utf8_buffer())
 		http_client.put_data(glb_data)
 	else:
-		var error_response = "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\nConnection: close\r\n\r\nGLB data not available."
-		http_client.put_data(error_response.to_utf8_buffer())
-
+		send_not_found(http_client, "GLB data not available.")
+	
 	http_client.disconnect_from_host()
+
+func send_bad_request(client, message):
+	var error_response = "HTTP/1.1 400 Bad Request\r\nContent-Type: text/plain\r\nConnection: close\r\n\r\n" + message
+	client.put_data(error_response.to_utf8_buffer())
+	client.disconnect_from_host()
+
+func send_not_found(client, message):
+	var error_response = "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\nConnection: close\r\n\r\n" + message
+	client.put_data(error_response.to_utf8_buffer())
+	client.disconnect_from_host()
